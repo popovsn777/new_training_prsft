242268529445150c6a4fee5bc4edbb8da4f07c92
popovsn777 <popovsn777@gmail.com>
2015-10-31 10:17:32 +0300

    change

---
+++ geom2d/geometry.py
@@ -0,0 +1,10 @@
+__author__ = 'Sergey'
+
+
+from geom2d import *
+
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1) ]
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1) ]
+
+
+print (l1 == l2)
---
+++ geom2d/point.py
@@ -0,0 +1,19 @@
+__author__ = 'Sergey'
+
+from math import sqrt
+
+print("name")
+
+class Point:
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
+
+
+    def distance(self, p2):
+        dx = p2.x - self.x
+        dy = p2.y - self.y
+        return sqrt(dx*dx + dy*dy)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
