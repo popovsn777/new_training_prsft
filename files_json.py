e34d90625409092d04441b84fb8fdec694f223c6
popovsn777 <popovsn777@gmail.com>
2015-11-05 11:30:09 +0300

    добавил функцию with

--- files_json.py
+++ files_json.py
@@ -3,13 +3,11 @@ __author__ = 'popov.sn'
 import json
 
 
-f = open("C:/python_barancev_w/new_training_prsft/config.json")
-try:
-    res = json.load(f)
-except ValueError as ex:
-    print(ex)
-    res = {}
-finally:
-    f.close()
+with open("C:/python_barancev_w/new_training_prsft/config.json") as f:
+    try:
+        res = json.load(f)
+    except ValueError as ex:
+        print(ex)
+        res = {}
 
 print(res)
